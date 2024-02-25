58d6c2767443c2098604ff549319124aa5c8f9e1
Abhishek Amralkar <abhishekamralkar@gmail.com>
2024-02-24 23:03:16 -0800

    [gowc] now support 2 flags count and size of the file

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 )
 
@@ -16,21 +16,54 @@ func getFileSize(filePath string) (int64, error) {
 	return fileInfo.Size(), nil
 }
 
+func getFileLines(filepath string) (int64, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return 0, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	lineCount := 0
+
+	for scanner.Scan() {
+		lineCount++
+	}
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+
+	return int64(lineCount), nil
+}
+
 func main() {
-	filePath := flag.String("c", "", "Path of the file")
 
-	flag.Parse()
+	fileSize := flag.String("c", "", "size of the file")
+	fileLines := flag.String("l", "", "line count in the file")
 
-	if *filePath == "" {
-		fmt.Println("Usage: ./ccwc -c filepath")
+	filePath := os.Args[2]
+
+	if len(os.Args) > 2 {
+		fmt.Println("Usage: gowc [options] filepath")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	filesize, err := getFileSize(*filePath)
-	if err != nil {
-		log.Fatal(err)
-	}
+	flag.Parse()
 
-	fmt.Println(filesize, *filePath)
+	switch {
+	case *fileSize != "":
+		f, _ := getFileSize(filePath)
+		fmt.Println(f, filePath)
+	case *fileLines != "":
+		s, _ := getFileLines(filePath)
+		fmt.Println(s, filePath)
+	default:
+		fmt.Println("Usage: gowc [options] filepath")
+		flag.PrintDefaults()
+		os.Exit(1)
+
+	}
 }
